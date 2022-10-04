387d3385189004e0506823de23f0e744c3b6d522
Alan Eicker <alaneicker@gmail.com>
2022-10-04 08:44:29 -0500

    Updates content and footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 type Props = {
   copyright: string;
+  emailAddress: string;
+  phoneNumber: string;
 };
 
-const Footer = ({ copyright }: Props) => (
+const Footer = ({ copyright, emailAddress, phoneNumber }: Props) => (
   <footer className="footer">
-    &copy;{' '}
-    {copyright.replace('[copyrightYear]', String(new Date().getFullYear()))}
+    <span>
+      &copy; {new Date().getFullYear()} {copyright} &bull;{' '}
+      <a href={`mailto:${emailAddress}`}>{emailAddress}</a> &bull; {phoneNumber}
+    </span>
   </footer>
 );
 
