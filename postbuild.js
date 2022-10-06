1cdec75c3bfb7087fa1abd11ae8658dca6815df6
Alan Eicker <alaneicker@gmail.com>
2022-10-06 10:10:14 -0500

    Refactoring

--- postbuild.js
+++
@@ -1,9 +0,0 @@
-const replace = require('replace');
-
-replace({
-  regex: new RegExp(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi),
-  replacement: '',
-  paths: ['dist/index.html'],
-  recursive: true,
-  silent: true,
-});
--- src/components/Career/Career.tsx
+++ src/components/Career/Career.tsx
@@ -4,13 +4,14 @@ import Section from '../Section';
 type Props = {
   title: string;
   content: string;
+  resumeUrl: string;
   companies: {
     company: string;
     duration: string;
   }[];
 };
 
-const Career = ({ companies, title, content, ...others }: Props) => (
+const Career = ({ companies, title, resumeUrl, content, ...others }: Props) => (
   <Section title={title} {...others}>
     <div className="career">
       <p>{content}</p>
@@ -21,7 +22,10 @@ const Career = ({ companies, title, content, ...others }: Props) => (
           </li>
         ))}
       </ul>
-    </>
+      <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
+        View my resum&eacute;
+      </a>
+    </div>
   </Section>
 );
 
