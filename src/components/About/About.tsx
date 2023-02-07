0858455fe407539472d34fc5e5e06b4ff39d5ea9
Alan Eicker <alaneicker@gmail.com>
2023-02-07 09:50:03 -0600

    Adds bio pic to About section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,15 +4,17 @@ import Section from '../Section';
 type Props = {
   title: string;
   content: string;
+  image: string;
 };
 
-const About = ({ title, content, ...others }: Props) => (
+const About = ({ image, title, content, ...others }: Props) => (
   <Section title={title} {...others}>
-    <>
+    <div className="about">
+      <img className="about__bio-pic" src={image} alt="Alan Eicker" />
       {content.split('|').map((paragraph, idx) => (
         <p key={idx + 1}>{paragraph}</p>
       ))}
-    </>
+    </div>
   </Section>
 );
 
