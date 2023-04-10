bda54b37e2059ada320758db75bb5d673db69632
Alan Eicker <alaneicker@gmail.com>
2023-04-10 11:16:32 -0500

    Updates

--- dist.52079508.js
+++ dist.52079508.js
@@ -24922,7 +24922,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58635" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64101" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
