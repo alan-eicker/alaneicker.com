597023557f7f5812959ce9ec968ab97ff1079550
Alan Eicker <alaneicker@gmail.com>
2023-04-11 09:16:13 -0500

    footer style updates

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import IconList from '../IconList';
 import { FooterProps } from '../../types/components';
 import './Footer.scss';
@@ -10,29 +11,42 @@ const Footer = ({
   socialLinks,
 }: FooterProps) => (
   <footer className="footer">
-    <IconList
-      label="find me on the web at"
-      items={socialLinks}
-      justify="center"
-      size={28}
-    />
-    <p>
-      Call me at <a href={`tel:${contactInfo.phone}`}>{contactInfo.phone}</a> or
-      email me at{' '}
-      <a href={`mailto:${contactInfo.email}`}>{contactInfo.email}</a>
-    </p>
-    <p>
-      &copy; {new Date().getFullYear()} {copyright}
-    </p>
-    <p>
-      <a target="_blank" href={iconCredits.iconUrl} rel="noreferrer">
-        {iconCredits.iconName}
-      </a>{' '}
-      icon by{' '}
-      <a target="_blank" href={iconCredits.websiteLinkUrl} rel="noreferrer">
-        {iconCredits.websiteLinkText}
-      </a>
-    </p>
+    <Grid>
+      <Row>
+        <Col md={6} className="footer__head">
+          <p>
+            Call me at{' '}
+            <a href={`tel:${contactInfo.phone}`}>{contactInfo.phone}</a> or
+            email me at{' '}
+            <a href={`mailto:${contactInfo.email}`}>{contactInfo.email}</a>
+          </p>
+          <p>
+            &copy; {new Date().getFullYear()} {copyright}
+          </p>
+          <p>
+            <a target="_blank" href={iconCredits.iconUrl} rel="noreferrer">
+              {iconCredits.iconName}
+            </a>{' '}
+            icon by{' '}
+            <a
+              target="_blank"
+              href={iconCredits.websiteLinkUrl}
+              rel="noreferrer"
+            >
+              {iconCredits.websiteLinkText}
+            </a>
+          </p>
+        </Col>
+        <Col md={6} className="footer__footer">
+          <IconList
+            label="find me on the web at"
+            items={socialLinks}
+            justify="center"
+            size={28}
+          />
+        </Col>
+      </Row>
+    </Grid>
   </footer>
 );
 
