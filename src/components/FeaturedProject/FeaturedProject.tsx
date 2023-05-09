d4511a048126b83f06671e6b4055688c63053dff
Alan Eicker <alaneicker@gmail.com>
2023-05-08 21:49:49 -0500

    more refactoring

--- src/components/FeaturedProject/FeaturedProject.tsx
+++ src/components/FeaturedProject/FeaturedProject.tsx
@@ -11,41 +11,39 @@ const FeaturedProject: React.FC<FeaturedProjectProps> = ({
   projectDescription,
   projectUrls,
 }) => (
-  <div className="featured-project">
-    <Grid>
-      <Row>
-        <Col md={3}>
-          <img
-            className="featured-project__project-image"
-            src={projectImage}
-            alt="featured project screenshot"
-            width="100%"
-          />
-        </Col>
-        <Col md={8} mdOffset={1}>
-          <h4>{sectionTitle}</h4>
-          <h5 className="featured-project__project-title">{projectTitle}</h5>
-          <h6 className="featured-project__project-subtitle">
-            {projectSubtitle}
-          </h6>
-          <p>{projectDescription}</p>
-          <div className="featured-project__links">
-            {projectUrls.map(({ href, text }) => (
-              <a
-                key={text}
-                className="btn-link"
-                href={href}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                {text}
-              </a>
-            ))}
-          </div>
-        </Col>
-      </Row>
-    </Grid>
-  </div>
+  <Grid className="featured-project">
+    <Row>
+      <Col md={3}>
+        <img
+          className="featured-project__project-image"
+          src={projectImage}
+          alt="featured project screenshot"
+          width="100%"
+        />
+      </Col>
+      <Col md={8} mdOffset={1}>
+        <h4>{sectionTitle}</h4>
+        <h5 className="featured-project__project-title">{projectTitle}</h5>
+        <h6 className="featured-project__project-subtitle">
+          {projectSubtitle}
+        </h6>
+        <p>{projectDescription}</p>
+        <div className="featured-project__links">
+          {projectUrls.map(({ href, text }) => (
+            <a
+              key={text}
+              className="btn-link"
+              href={href}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              {text}
+            </a>
+          ))}
+        </div>
+      </Col>
+    </Row>
+  </Grid>
 );
 
 export default FeaturedProject;
